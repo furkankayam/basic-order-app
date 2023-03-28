ee820d9bee73b02d374f24a609b218404625abf7
Furkan Kaya <furkan.36kaya@gmail.com>
2023-03-28 05:20:51 +0300

    feat: initial commit

---
+++ src/main/java/com/orderapp/BasicOrderAppApplication.java
@@ -0,0 +1,13 @@
+package com.orderapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicOrderAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicOrderAppApplication.class, args);
+    }
+
+}
